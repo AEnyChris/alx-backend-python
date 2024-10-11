45d732adaa2127a6cbee91f55d1d95d659302bc0
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-11 11:25:17 +0100

    added task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+type annotation for a simple function
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''concats two strings and returns a string'''
+    return str1 + str2
