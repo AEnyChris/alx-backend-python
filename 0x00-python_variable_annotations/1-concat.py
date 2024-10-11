b9d3e3f64a2900b5bfaccc866ef45020f4d14434
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-11 11:41:15 +0100

    added specified shebang

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''
 type annotation for a simple function
 '''
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+type annotation for a simple function
+'''
+
+
+def floor(n: floor) -> int:
+    '''returns the floor of n'''
+    import math
+    return math.floor(n)
