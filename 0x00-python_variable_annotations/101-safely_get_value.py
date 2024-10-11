6bea2166a5bbca6d29af0441820d1ec3a95d1e6a
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-11 15:22:23 +0100

    added task 101

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+More involved type annotations
+'''
+from typing import TypeVar, Union, Any, Mapping
+
+
+T = TypeVar('T')
+dt = Union[T, None]
+rt = Union[Any, T]
+
+def safely_get_value(dct: Mapping, key: Any, default: dt = None) -> rt:
+    '''safely get value of key or none'''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
