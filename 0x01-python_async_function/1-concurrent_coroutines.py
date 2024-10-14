925ceeb063001d5bb8c5c9bc889594f28871ae7f
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-14 23:51:56 +0100

    corrections

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,5 +10,7 @@ wait_ran = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''returns a list of the outputs of wait_random'''
-    res = await asyncio.gather(*(wait_ran(max_delay) for i in range(n)))
+    res = await asyncio.gather(
+        *tuple(map(lambda _: wait_ran(max_delay), range(n)))
+    )
     return sorted(res)
