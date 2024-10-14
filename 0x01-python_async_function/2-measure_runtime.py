0ba2b8453e72b033aad58d4c0ea9788aa4020476
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-15 00:32:56 +0100

    corrections

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,6 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """returns the elapse time of a coroutine call"""
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     elapse = time.perf_counter() - start
