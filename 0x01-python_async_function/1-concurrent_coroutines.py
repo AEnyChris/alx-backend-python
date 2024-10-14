2071af1f07548772873ee9d0ed739c8d410071c4
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-14 23:24:28 +0100

    corrections

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-'''A function as a Chained coroutines'''
+'''
+A function as a Chained coroutines
+'''
 import asyncio
-import random
 
 
 wait_ran = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> list:
     '''returns a list of the outputs of wait_random'''
     res = [await wait_ran(max_delay) for i in range(n)]
     return res
