f550d7838e418ff61461071e94a3b17e95a7cb0b
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-15 22:16:32 +0100

    correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """returns a random number between 0 and 10 but float"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
