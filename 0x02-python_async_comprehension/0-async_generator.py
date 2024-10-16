bc51af2008db9d9b087148f92ab72efa920ab897
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-16 08:37:45 +0100

    correction on type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Defines an asynchronous generator"""
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """returns a random number between 0 and 10 but float"""
+async def async_generator() -> Generator[float, None, None]:
+    """returns a list of random float numbers between 0 and 10"""
     for i in range(10):
         yield random.random() * 10
         await asyncio.sleep(1)
